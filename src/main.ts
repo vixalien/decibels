641349c3b513fef4653620b6aef0a77eef396dcb
Angelo Verlain <geoangercola@gmail.com>
2023-09-19 19:55:05 +0200

    use `Gtk.Application.run` instead of `runAsync`

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,5 @@ export function main(argv: string[]): Promise<number> {
   const app = new Application();
   // @ts-expect-error gi.ts can't generate this, but it exists.
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
-  return app.runAsync(argv);
+  return app.run(argv);
 }
