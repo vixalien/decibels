2b6c769203828eec9d8b192332e72d7293d7c16e
David Keller <davidkeller@tuta.io>
2023-12-16 17:43:09 +0100

    Add myself to credit section

--- src/application.ts
+++ src/application.ts
@@ -8,74 +8,75 @@ import { Window } from "./window.js";
 export const Settings = new Gio.Settings({ schema: pkg.name });
 
 export class Application extends Adw.Application {
-  private window?: Window;
+    private window?: Window;
 
-  static {
-    GObject.registerClass(this);
-  }
-
-  constructor() {
-    super({
-      application_id: pkg.name,
-      resource_base_path: "/com/vixalien/decibels",
-      flags: Gio.ApplicationFlags.HANDLES_OPEN,
-    });
-
-    const quit_action = new Gio.SimpleAction({ name: "quit" });
-    quit_action.connect("activate", () => {
-      this.quit();
-    });
-
-    this.add_action(quit_action);
-    this.set_accels_for_action("app.quit", ["<Control>q"]);
-
-    this.set_accels_for_action("win.open-file", ["<Control>o"]);
-
-    const show_about_action = new Gio.SimpleAction({ name: "about" });
-    show_about_action.connect("activate", () => {
-      const aboutWindow = Adw.AboutWindow.new_from_appdata(
-        "/com/vixalien/decibels/com.vixalien.decibels.metainfo.xml",
-        // remove commit tag
-        pkg.version.split("-")[0],
-      );
-      aboutWindow.set_version(pkg.version);
-      aboutWindow.set_developers([
-        "Angelo Verlain https://vixalien.com",
-      ]);
-      aboutWindow.set_artists(["kramo https://kramo.hu"]);
-      aboutWindow.set_designers(["Allan Day"]);
-      aboutWindow.set_transient_for(this.get_active_window());
-      /* Translators: Replace "translator-credits" with your names, one name per line */
-      aboutWindow.set_translator_credits(_("translator-credits"));
-
-      aboutWindow.present();
-    });
-
-    this.add_action(show_about_action);
-
-    Gio._promisify(Gtk.UriLauncher.prototype, "launch", "launch_finish");
-  }
-
-  private present_main_window(): void {
-    if (!this.window) {
-      this.window = new Window({ application: this });
-      if (pkg.name.endsWith("Devel")) this.window.add_css_class("devel");
+    static {
+        GObject.registerClass(this);
     }
 
-    this.window.present();
-  }
+    constructor() {
+        super({
+            application_id: pkg.name,
+            resource_base_path: "/com/vixalien/decibels",
+            flags: Gio.ApplicationFlags.HANDLES_OPEN,
+        });
 
-  vfunc_activate(): void {
-    this.present_main_window();
-  }
+        const quit_action = new Gio.SimpleAction({ name: "quit" });
+        quit_action.connect("activate", () => {
+            this.quit();
+        });
 
-  vfunc_open(files: Gio.FilePrototype[], hint: string): void {
-    this.present_main_window();
+        this.add_action(quit_action);
+        this.set_accels_for_action("app.quit", ["<Control>q"]);
 
-    const window = this.get_active_window();
+        this.set_accels_for_action("win.open-file", ["<Control>o"]);
 
-    if (window && window instanceof Window && files.length > 0) {
-      window.load_file(files[0]);
+        const show_about_action = new Gio.SimpleAction({ name: "about" });
+        show_about_action.connect("activate", () => {
+            const aboutWindow = Adw.AboutWindow.new_from_appdata(
+                "/com/vixalien/decibels/com.vixalien.decibels.metainfo.xml",
+                // remove commit tag
+                pkg.version.split("-")[0],
+            );
+            aboutWindow.set_version(pkg.version);
+            aboutWindow.set_developers([
+                "Angelo Verlain https://vixalien.com",
+                "David Keller https://gitlab.com/BlobCodes"
+            ]);
+            aboutWindow.set_artists(["kramo https://kramo.hu"]);
+            aboutWindow.set_designers(["Allan Day"]);
+            aboutWindow.set_transient_for(this.get_active_window());
+            /* Translators: Replace "translator-credits" with your names, one name per line */
+            aboutWindow.set_translator_credits(_("translator-credits"));
+
+            aboutWindow.present();
+        });
+
+        this.add_action(show_about_action);
+
+        Gio._promisify(Gtk.UriLauncher.prototype, "launch", "launch_finish");
     }
-  }
+
+    private present_main_window(): void {
+        if (!this.window) {
+            this.window = new Window({ application: this });
+            if (pkg.name.endsWith("Devel")) this.window.add_css_class("devel");
+        }
+
+        this.window.present();
+    }
+
+    vfunc_activate(): void {
+        this.present_main_window();
+    }
+
+    vfunc_open(files: Gio.FilePrototype[], hint: string): void {
+        this.present_main_window();
+
+        const window = this.get_active_window();
+
+        if (window && window instanceof Window && files.length > 0) {
+            window.load_file(files[0]);
+        }
+    }
 }
