6a14c069d26c33032a4cfdf0d53c2b808c70e205
David Keller <davidkeller@tuta.io>
2023-12-21 14:27:26 +0100

    Add basic keyboard controls (#48)
    * Add basic keyboard controls
    
    * Do not propagate key event once handled in player
    
    * Apply suggestions from @vixalien
    
    * Move GtkEventControllerKey to player top-level
    
    * Reset the flatpak build file
    
    * Remove useless property on APWaveForm
    
    * Fixed identation on flatpak metadata

--- src/player.ts
+++ src/player.ts
@@ -224,6 +224,30 @@ export class APPlayerState extends Adw.Bin {
     stream.seek(d);
   }
 
+  private event_key_pressed(
+    _controller: Gtk.EventControllerKey,
+    keyval: number,
+    _keycode: number,
+    _modifier: Gdk.ModifierType
+  ): boolean {
+    const window = this.get_root() as Window;
+    const stream = window?.stream;
+
+    if (!stream) return Gdk.EVENT_PROPAGATE;
+
+    if (keyval === Gdk.KEY_space) {
+      stream.playing ? stream.pause() : stream.play();
+    } else if (keyval === Gdk.KEY_Left) {
+      stream.skip_seconds(-10);
+    } else if (keyval === Gdk.KEY_Right) {
+      stream.skip_seconds(10);
+    } else {
+      return Gdk.EVENT_PROPAGATE;
+    }
+
+    return Gdk.EVENT_STOP;
+  }
+
   private waveform_position_changed_cb(_scale: Gtk.Scale, value: number) {
     const window = this.get_root() as Window;
     const stream = window?.stream;
